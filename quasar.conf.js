331b1ceb9cd6ffc646aff56b9230a2d93ebfbd94
Kira076 <31233105+Kira076@users.noreply.github.com>
2021-09-28 00:42:26 -0400

    Changed build location

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,6 +47,8 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
+	  publicPath: '../quasar.chaotibot.com/',
+	  distDir: '../quasar.chaotibot.com/',
 
       // transpile: false,
 
